43dd15c032b66b206e1285dc2e5a9829f5106d9a
ziadmustafamuhammed <163066827+ziadmustafamuhammed@users.noreply.github.com>
2026-02-03 02:30:27 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export default async function handler(req, res) {
     try {
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
         const prompt = req.body.prompt;
         const result = await model.generateContent(prompt);
